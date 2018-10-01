549ccd5bc3c1a8c9d850928e1a3fb52a118404ec
oreculf <oreculf@gmail.com>
2018-09-30 17:52:28 -0700

    added sample routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Link } from "@reach/router";
+import { Link, Router } from "@reach/router";
 import "./index.scss";
 
 const App = () => (
@@ -9,7 +9,44 @@ const App = () => (
     <nav>
       <Link to="/">Home</Link>
       <Link to="search">Search</Link>
+      <Link to="profile">Profile</Link>
+      <Link to="login">Login</Link>
+      <Link to="signup">Signup</Link>
     </nav>
+
+    <Router>
+      <Home path="/" />
+      <Search path="/search" />
+      <Profile name="oreculf" path="/profile" />
+      <Login path="/login" />
+      <Signup path="/signup" />
+    </Router>
+  </div>
+);
+
+const Home = () => (
+  <div>
+    <h2>Welcome</h2>
+  </div>
+);
+const Search = () => (
+  <div>
+    <h2>Search</h2>
+  </div>
+);
+const Profile = ({ name }) => (
+  <div>
+    <h2>Welcome {name}</h2>
+  </div>
+);
+const Login = () => (
+  <div>
+    <h2>Login</h2>
+  </div>
+);
+const Signup = () => (
+  <div>
+    <h2>Signup</h2>
   </div>
 );
 
