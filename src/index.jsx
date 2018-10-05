520a763c35b44e4df46de43ae5fabf503316011c
oreculf <oreculf@gmail.com>
2018-10-04 21:50:06 -0700

    created login component

--- src/index.jsx
+++ src/index.jsx
@@ -39,11 +39,44 @@ const Profile = ({ name }) => (
     <h2>Welcome {name}</h2>
   </div>
 );
-const Login = () => (
-  <div>
-    <h2>Login</h2>
-  </div>
-);
+// const Login = () => (
+//   <div>
+//     <h2>Login</h2>
+//   </div>
+// );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div>
+        <p>Username: {username}</p>
+        <input
+          type="text"
+          value={username}
+          onChange={event => {
+            this.setState({ username: event.target.value });
+          }}
+        />
+        <p>Password: {password}</p>
+        <input
+          type="password"
+          value={password}
+          onChange={event => {
+            this.setState({ password: event.target.value });
+          }}
+        />
+      </div>
+    );
+  }
+}
 const Signup = () => (
   <div>
     <h2>Signup</h2>
